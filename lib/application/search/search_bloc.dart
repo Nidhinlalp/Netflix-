ea6ea869339ad60a667ac48e3d4f59e40eccb743
Nidhin <lalnidhinp02@gmail.com>
2023-02-09 12:32:39 +0530

    search initialized

--- lib/application/search/search_bloc.dart
+++ lib/application/search/search_bloc.dart
@@ -1,14 +1,33 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:netflix/domain/downloads/i_downloads_repo.dart';
+import 'package:netflix/domain/downloads/models/downloads.dart';
+import 'package:netflix/domain/search/model/search_rep/search_rep.dart';
+import 'package:netflix/domain/search/model/search_rep/search_servivce.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
 part 'search_bloc.freezed.dart';
 
+@injectable
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
-  SearchBloc() : super(_Initial()) {
-    on<SearchEvent>((event, emit) {
-      // TODO: implement event handler
+  final IDownloadsRepo downloadsService;
+  final SearchService searchService;
+  SearchBloc(this.downloadsService, this.searchService)
+      : super(SearchState.Initial()) {
+//idle state  //////////////////////////////////
+    on<Initialize>((event, emit) {
+      //get trending
+      downloadsService.getDownloadsImage(); //show to ui
+    });
+
+    //search result state//////////////////////////////
+
+    on<SearchMovie>((event, emit) {
+      //call search movie api
+      searchService.searchMovies(movieQuery: event.movieQuery);
+      //show to ui
     });
   }
 }
--- lib/presentation/search/scren_search.dart
+++ lib/presentation/search/scren_search.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:netflix/application/search/search_bloc.dart';
 import 'package:netflix/core/colors/colors.dart';
 import 'package:netflix/core/size/constsaize.dart';
 import 'package:netflix/presentation/search/widget/search_idile.dart';
@@ -13,6 +15,9 @@ class ScreenSearch extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      BlocProvider.of<SearchBloc>(context).add(const Initialize());
+    });
     return Scaffold(
       body: SafeArea(
         child: Padding(
