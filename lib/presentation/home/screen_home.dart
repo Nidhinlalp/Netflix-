19245ec2322c6efca9424739296c5e957292f742
Nidhin <lalnidhinp02@gmail.com>
2023-02-11 16:02:21 +0530

    formation

--- lib/presentation/home/screen_home.dart
+++ lib/presentation/home/screen_home.dart
@@ -22,50 +22,34 @@ class ScreenHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: ValueListenableBuilder(
-          valueListenable: homefirstimage,
-          builder: (context, homeImage, _) {
-            return homeImage.isEmpty
-                ? const Center(child: CircularProgressIndicator())
-                : ValueListenableBuilder(
-                    valueListenable: scrollNotifire,
-                    builder: (BuildContext context, index, _) {
-                      return NotificationListener<UserScrollNotification>(
-                        onNotification: (notification) {
-                          final ScrollDirection direction =
-                              notification.direction;
-                          if (direction == ScrollDirection.reverse) {
-                            scrollNotifire.value = false;
-                          } else if (direction == ScrollDirection.forward) {
-                            scrollNotifire.value = true;
-                          }
-                          return true;
-                        },
-                        child: Stack(
-                          children: [
-                            ListView(
-                              children: [
-                                ValueListenableBuilder(
-                                    valueListenable: homefirstimage,
-                                    builder: (context, result, _) {
-                                      if (result.isNotEmpty) {
-                                        return HomeFirstContainer(
-                                          imageUrl: result[0].bagroudimage,
-                                        );
-                                      }
-                                      return const Center(
-                                        child: CircularProgressIndicator(
-                                          strokeWidth: 2,
-                                        ),
-                                      );
-                                    }),
-                                khight,
-                                ValueListenableBuilder(
-                                  valueListenable: relesethePastYear,
-                                  builder: (context, reslt, _) {
-                                    if (reslt.isNotEmpty) {
-                                      return MainTitleCard(
-                                        title: "Released in the past year",
-                                        result: reslt,
+        valueListenable: homefirstimage,
+        builder: (context, homeImage, _) {
+          return homeImage.isEmpty
+              ? const Center(child: CircularProgressIndicator())
+              : ValueListenableBuilder(
+                  valueListenable: scrollNotifire,
+                  builder: (BuildContext context, index, _) {
+                    return NotificationListener<UserScrollNotification>(
+                      onNotification: (notification) {
+                        final ScrollDirection direction =
+                            notification.direction;
+                        if (direction == ScrollDirection.reverse) {
+                          scrollNotifire.value = false;
+                        } else if (direction == ScrollDirection.forward) {
+                          scrollNotifire.value = true;
+                        }
+                        return true;
+                      },
+                      child: Stack(
+                        children: [
+                          ListView(
+                            children: [
+                              ValueListenableBuilder(
+                                  valueListenable: homefirstimage,
+                                  builder: (context, result, _) {
+                                    if (result.isNotEmpty) {
+                                      return HomeFirstContainer(
+                                        imageUrl: result[0].bagroudimage,
                                       );
                                     }
                                     return const Center(
@@ -73,129 +57,147 @@ class ScreenHome extends StatelessWidget {
                                         strokeWidth: 2,
                                       ),
                                     );
-                                  },
-                                ),
-                                khight,
-                                ValueListenableBuilder(
-                                    valueListenable: trendingNow,
-                                    builder: (context, trendingNow, _) {
-                                      if (trendingNow.isNotEmpty) {
-                                        return MainTitleCard(
-                                          title: "Trending Now",
-                                          result: trendingNow,
-                                        );
-                                      }
-                                      return const Center(
-                                        child: CircularProgressIndicator(
-                                          strokeWidth: 2,
-                                        ),
-                                      );
-                                    }),
-                                khight,
-                                ValueListenableBuilder(
-                                    valueListenable: top10showindia,
-                                    builder:
-                                        (context, resultofsoutindianmovie, _) {
-                                      if (resultofsoutindianmovie.isNotEmpty) {
-                                        return NumberTitleCard(
-                                          result: resultofsoutindianmovie,
-                                        );
-                                      }
-                                      return const Center(
-                                        child: CircularProgressIndicator(
-                                          strokeWidth: 2,
-                                        ),
-                                      );
-                                    }),
-                                khight,
-                                ValueListenableBuilder(
-                                    valueListenable: tendeAndDrama,
-                                    builder: (context, resultTenseDrama, _) {
-                                      if (resultTenseDrama.isNotEmpty) {
-                                        return MainTitleCard(
-                                          title: " Tense Dramas",
-                                          result: resultTenseDrama,
-                                        );
-                                      }
-                                      return const Center(
-                                        child: CircularProgressIndicator(
-                                          strokeWidth: 2,
-                                        ),
-                                      );
-                                    }),
-                                khight,
-                                ValueListenableBuilder(
-                                    valueListenable: southindianmovie,
-                                    builder:
-                                        (context, resultodfsutindianmovie, _) {
-                                      if (resultodfsutindianmovie.isNotEmpty) {
-                                        return MainTitleCard(
-                                          title: "Sout indian Cinema",
-                                          result: resultodfsutindianmovie,
-                                        );
-                                      }
-                                      return const Center(
-                                        child: CircularProgressIndicator(
-                                          strokeWidth: 2,
-                                        ),
-                                      );
-                                    }),
-                              ],
-                            ),
-                            scrollNotifire.value == true
-                                ? AnimatedContainer(
-                                    duration:
-                                        const Duration(milliseconds: 1500),
-                                    width: double.infinity,
-                                    height: 80,
-                                    color: Colors.black.withOpacity(0.3),
-                                    child: Column(
-                                      children: [
-                                        Row(
-                                          children: [
-                                            Image.network(
-                                              'https://proofmart.com/wp-content/uploads/2021/06/598-2-1.png',
-                                              width: 50,
-                                              height: 50,
-                                            ),
-                                            const Spacer(),
-                                            const Icon(
-                                              Icons.cast_outlined,
-                                              size: 30,
-                                              color: kwhite,
-                                            ),
-                                            kwidth,
-                                            const NetflixProfile(),
-                                          ],
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.spaceEvenly,
-                                          children: [
-                                            Text(
-                                              'TV Shows',
-                                              style: kHometext,
-                                            ),
-                                            Text(
-                                              'Movies',
-                                              style: kHometext,
-                                            ),
-                                            Text(
-                                              'Categories',
-                                              style: kHometext,
-                                            ),
-                                          ],
-                                        )
-                                      ],
+                                  }),
+                              khight,
+                              ValueListenableBuilder(
+                                valueListenable: relesethePastYear,
+                                builder: (context, reslt, _) {
+                                  if (reslt.isNotEmpty) {
+                                    return MainTitleCard(
+                                      title: "Released in the past year",
+                                      result: reslt,
+                                    );
+                                  }
+                                  return const Center(
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2,
                                     ),
-                                  )
-                                : khight
-                          ],
-                        ),
-                      );
-                    },
-                  );
-          }),
+                                  );
+                                },
+                              ),
+                              khight,
+                              ValueListenableBuilder(
+                                valueListenable: trendingNow,
+                                builder: (context, trendingNow, _) {
+                                  if (trendingNow.isNotEmpty) {
+                                    return MainTitleCard(
+                                      title: "Trending Now",
+                                      result: trendingNow,
+                                    );
+                                  }
+                                  return const Center(
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                    ),
+                                  );
+                                },
+                              ),
+                              khight,
+                              ValueListenableBuilder(
+                                valueListenable: top10showindia,
+                                builder: (context, resultofsoutindianmovie, _) {
+                                  if (resultofsoutindianmovie.isNotEmpty) {
+                                    return NumberTitleCard(
+                                      result: resultofsoutindianmovie,
+                                    );
+                                  }
+                                  return const Center(
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                    ),
+                                  );
+                                },
+                              ),
+                              khight,
+                              ValueListenableBuilder(
+                                valueListenable: tendeAndDrama,
+                                builder: (context, resultTenseDrama, _) {
+                                  if (resultTenseDrama.isNotEmpty) {
+                                    return MainTitleCard(
+                                      title: " Tense Dramas",
+                                      result: resultTenseDrama,
+                                    );
+                                  }
+                                  return const Center(
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                    ),
+                                  );
+                                },
+                              ),
+                              khight,
+                              ValueListenableBuilder(
+                                valueListenable: southindianmovie,
+                                builder: (context, resultodfsutindianmovie, _) {
+                                  if (resultodfsutindianmovie.isNotEmpty) {
+                                    return MainTitleCard(
+                                      title: "Sout indian Cinema",
+                                      result: resultodfsutindianmovie,
+                                    );
+                                  }
+                                  return const Center(
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                          scrollNotifire.value == true
+                              ? AnimatedContainer(
+                                  duration: const Duration(milliseconds: 1500),
+                                  width: double.infinity,
+                                  height: 80,
+                                  color: Colors.black.withOpacity(0.3),
+                                  child: Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Image.network(
+                                            'https://proofmart.com/wp-content/uploads/2021/06/598-2-1.png',
+                                            width: 50,
+                                            height: 50,
+                                          ),
+                                          const Spacer(),
+                                          const Icon(
+                                            Icons.cast_outlined,
+                                            size: 30,
+                                            color: kwhite,
+                                          ),
+                                          kwidth,
+                                          const NetflixProfile(),
+                                        ],
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceEvenly,
+                                        children: [
+                                          Text(
+                                            'TV Shows',
+                                            style: kHometext,
+                                          ),
+                                          Text(
+                                            'Movies',
+                                            style: kHometext,
+                                          ),
+                                          Text(
+                                            'Categories',
+                                            style: kHometext,
+                                          ),
+                                        ],
+                                      )
+                                    ],
+                                  ),
+                                )
+                              : khight
+                        ],
+                      ),
+                    );
+                  },
+                );
+        },
+      ),
     );
   }
 }
